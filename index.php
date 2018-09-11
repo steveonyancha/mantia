21e84044deba57e4c36b6c73ca7b74ec723fb248
blackimpala <stivinmo@gmail.com>
2018-09-11 14:59:07 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" >
+    <!-- proposed site for pharmacy management system- Simple and effective - -->
+  </head>
+  <body>
+  </body>
+</html>
